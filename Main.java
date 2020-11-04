118719effe971adaffbec48d40232af11cddd3ab
pajid9763 <pajid9763@wrdsb.ca>
2020-11-04 22:27:34 +0000

    middle of VCPD act

--- Main.java
+++ Main.java
@@ -25,6 +25,7 @@ public class Main implements Runnable, ActionListener{
 
   // Method to assemble our GUI
   public void run(){
+    
     // Creats a JFrame that is 800 pixels by 600 pixels, and closes when you click on the X
     JFrame frame = new JFrame("Valor City 2077");
     // Makes the X button close the program
@@ -49,6 +50,8 @@ public class Main implements Runnable, ActionListener{
     // set the size and location of the label 
     roleLabel.setBounds(300, 10, 200, 20);
 
+    roleLabel.setForeground(Color.red);
+
     // add the label to the main panel 
     gameScreen.add(roleLabel);
 
@@ -59,11 +62,14 @@ public class Main implements Runnable, ActionListener{
     // set the size and location of the JTextArea 
     storyText.setBounds(30, 60, 740, 240);
 
+    // wrap text in storyText
+    storyText.setLineWrap(true);
+
     // disable the text area so the user can't type inside it 
     storyText.setEnabled(false);
 
     // change the text color in storyText
-    Font font = new Font ("Times New Roman", Font.PLAIN, 22);
+    Font font = new Font ("Times New Roman", Font.PLAIN, 20);
     storyText.setForeground(Color.red);
     storyText.setFont(font);
     
@@ -166,6 +172,7 @@ public class Main implements Runnable, ActionListener{
     
 
   }
+  
     // create methods for different choices 
     public void streetThug(){
       // declare what the marker will be so it can be called in a switch
@@ -189,37 +196,105 @@ public class Main implements Runnable, ActionListener{
       marker = "continue";
       
       choiceButtons[2].setVisible(true);
-      choiceButtons[3].setVisible(true);
-      choiceButtons[4].setVisible(true);
+      choiceButtons[3].setVisible(false);
+      choiceButtons[4].setVisible(false);
       
-      storyText.setText("The phone is ringing\nYou pick it up, and to your surprise, no caller ID\nHesitant, you answer\n\"You're Ripper, right?\" Says the man on the other side\n");
+      storyText.setText("The phone is ringing\nYou pick it up, and to your surprise, no caller ID\nHesitant, you answer\n\"You're Ripper, right?\" Says the man on the other side\n\"Yes... who is this calling\" You say with a hint of concern\n\"That's not important, just come to Little Tokyo if you're about it,\nand come alone\"\n\"Wait you don't mean -\" The mysterious man ended the call before you\ncould finish ");
 
-      choiceButtons[1].setText("1");
-      choiceButtons[2].setText("2");
-      choiceButtons[3].setText("3");
-      choiceButtons[4].setText("4");
+      choiceButtons[1].setText("<html><center>"+"Go meet him in"+"<br>"+"Little Tokyo alone"+"</center></html>Go meet him in Little Tokyo alone");
+      choiceButtons[2].setText("<html><center>"+"Go meet him in"+"<br>"+"Little Tokyo with a group"+"</center></html>");
     }
 
-    public void test(){
-      marker = "test";
+    public void meetAlone(){
+      marker = "meetAlone";
       
-      storyText.setText("hello");
-      choiceButtons[1].setText("hey");
+      storyText.setText("You go to Little Tokyo by yourself. When you get there you cannot believe your eyes. The man standing infront of you is the famous Flash Fang.\nKnown for being incredibly fast and as ferocious as a wolf. He's basically what every one of us lowly thugs want to become.\n\"Flash Fang, i- what an honour to be in your presen-\"\n\"Cut the small talk Ripper, I'm building a team to perform the greatest\nheist Valor City has ever seen. We're gonna steal the chip of immortality and I will then become the king that city has always needed!\" ");
+      
+      choiceButtons[1].setText("Say no more, I'm in!");
 
+      choiceButtons[2].setVisible(false);
     }
 
-    public void test2(){
-      marker = "test2";
+    public void meetGroup(){
+      marker = "meetGroup";
       
-      storyText.setText(" this is a test");
+      storyText.setText("\"You really don't know how to follow orders boy,\nI can't have an airhead like you on my team! Get lost before you regret\nit!\"");
 
-      choiceButtons[1].setText("test2");
-      choiceButtons[2].setText("test3");
-      choiceButtons[3].setText("test27");
-      choiceButtons[4].setText("test5");
+      choiceButtons[1].setText("Resart");
+
+      choiceButtons[2].setVisible(false);
+      choiceButtons[3].setVisible(false);
+      choiceButtons[4].setVisible(false); 
+    }
+
+    public void testJoin(){
+      marker = "test to join";
+
+      storyText.setText(" \"Slow down kid, your skills look good on paper but I'm not just gonna\ngive you the spot without seeing you in action. You're gonna do\nsomething for me and then we'll see.\" Flash Fang proclaimed\n\"What needs to be done to join?\" You ask\n\"You see, in a little shootout last week, I lost my 3 katanas to the VCPD\n(Valor City Police Department) and I need you to retrieve them. That\nsound like something you can do?\"");
+
+      choiceButtons[1].setText("I'll be back in no time");
+      choiceButtons[2].setText("I have a better idea");
+
+      choiceButtons[2].setVisible(true);
     }
     
+    public void stealPolice(){
+      marker = "steal from police";
+
+      storyText.setText(" You say your goodbyes to Flash Fang and the rest of his crew as you\ndepart for the VCPD's main headquarters. Flash provided you with a\nmapped out layout of the HQ, all you have to do is follow this map to\nwhere the storage area is and find Flash Fang's 3 katanas.\nThe plan is simple, the easiest way to get inside unnoticed is if you act\nlike one of them. In order for that to work you will need a VCPD outfit.\nYou stake out outside of the building until someone comes out.\nFinally after half an hour of waiting it looks like an officer has left the\nbuilding.");
+
+      choiceButtons[1].setText("Follow the officer");
+      choiceButtons[2].setVisible(false);
+
+    }
+
+    public void streetRace(){
+      marker = "street race";
+
+      storyText.setText(" \"You have a better a idea, let's hear it then!\"Demands Flash\n\"Hear me out, there's a street race happening not too far from here on\nHollow Street, some of the biggest betters are rumoured to join in on the fun. Whaddya say I enter, win the damn thing and you'll have more than enough money to buy yourself the best looking katanas on the market?\" You say confidentally\n\"Well if you're so confident, go for it, but if you don't win. Boy you won't wanna know the consequences!\"Flash said with an evil look in his eyes.\nAnd with that you were off to Hollow Street.");
+
+      choiceButtons[1].setText("Arrive at Hollow Street");
+      choiceButtons[2].setVisible(false);
+    }
+
+    public void followOfficer(){
+      marker = "follow officer";
+
+      storyText.setText("You start to follow the officer. You then figure out the general path he\nwill be taking and move ahead of him to intercept him. The area that you have chosen is pratically a ghost town, no one in sight. You hide behind one of the buildings. The officer is starting to get closer. What do you do?");
+
+      choiceButtons[2].setVisible(true);
+
+      choiceButtons[1].setText("<html><center>"+"Charge at the officer to"+"<br>"+"try to get the jump on him"+"</center></html>");
+      choiceButtons[2].setText("<html><center>"+"Wait patiently for him"+"<br>"+"to turn the corner"+"</center></html>");
+      
+    }
     
+    public void chargeAttack(){
+      marker = "charge attack";
+
+      storyText.setText("You charge at the officer, trying to get an early knockout on him,but his reflexes are too quick. Before you can even get remotely close to him he pulls a gun on you.\n\"What the hell do you think you're doing?\" The officer asks.\nThink fast, do you throw a smokescreen or do you pelt him in the head with a rock?");
+
+      choiceButtons[1].setText("Smokescreen");
+      choiceButtons[2].setText("Throw rock");
+    }
+
+    public void throwRock(){
+      marker = "throw rock";
+
+      storyText.setText(" You have a pretty big looking rock in your right hand behind your back.\nYou quickly hurl it at the officer. His reflexes kick in once again and he manages to safely dodge it.\nWithout a second thought he pulls the trigger");
+
+      choiceButtons[1].setText("Game Over!");
+      choiceButtons[2].setVisible(false);
+    }
+
+    public void gameOver(){
+      marker = "game over";
+
+      storyText.setText("Tough luck, guess it just wasn't your day!\nHere, try again");
+
+      choiceButtons[1].setText("Restart");
+    }
+
 
   // method called when a button is pressed
   public void actionPerformed(ActionEvent e){
@@ -241,17 +316,57 @@ public class Main implements Runnable, ActionListener{
         break;
     case "continue":
         switch (command){
-        case "choice1": test(); break;
-        case "choice2": test(); break;
-        case "choice3": test(); break;
-        case "choice4": test(); break;
+        case "choice1": meetAlone(); break;
+        case "choice2": meetGroup(); break;
         }
         break;
-    case "test":
+    case "meetAlone":
         switch (command){
-          case "choice1": test2(); break;  
+          case "choice1": testJoin(); break;  
         }
         break;
+    case "meetGroup":
+        switch (command){
+          case "choice1": streetThug(); break;
+        }
+        break;
+    case "test to join":
+        switch (command){
+          case "choice1": stealPolice(); break;
+          case "choice2": streetRace(); break;
+        }
+        break;
+    case "steal from police": 
+        switch (command){
+          case "choice1": followOfficer(); break;
+        }
+        break;
+    case "street race":
+        switch (command){
+          case "choice1": break;
+        }
+        break;
+    case "follow officer":
+        switch(command){
+          case "choice1": chargeAttack(); break;
+          case "choice2":  break;
+        }
+        break;
+    case "charge attack":
+        switch (command){
+          case "choice1": break;
+          case "choice2": throwRock(); break;
+        }
+        break;
+    case "throw rock":
+        switch (command){
+          case "choice1": gameOver();
+        }
+        break;
+    case "game over":
+        switch (command){
+          case "choice1": streetThug();
+        }
    }
   }
   
